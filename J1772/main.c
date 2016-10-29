5b3400bbc8e7fa7d2930fd8ddd6c3539e5849eb9
Florian Uhlemann <florianuhlemann@florianuhlemann.local>
2016-10-29 15:25:51 -0400

    Initial Commit

---
+++ J1772/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  J1772
+//
+//  Created by Florian Uhlemann on 29.10.16.
+//  Copyright © 2016 Florian Uhlemann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
